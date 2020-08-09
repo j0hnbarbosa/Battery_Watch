7e77b8abd5d9321713c827cdf1294de100528f79
j0hnbarbosa <john.contatob@gmail.com>
2020-08-09 18:38:07 -0300

    added scrol

--- app/src/main/java/com/example/batery_watch/MainActivity.kt
+++ app/src/main/java/com/example/batery_watch/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.batery_watch
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.method.ScrollingMovementMethod
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +12,12 @@ import java.io.InputStream
 
 
 class MainActivity : AppCompatActivity() {
+
+
+    val time: Long = 1000000000L
+    var timer = Timer(time)
+    var isStop = false
+
 
     fun readFile() {
 
@@ -22,24 +30,68 @@ class MainActivity : AppCompatActivity() {
 
 
             val inputString = inputStream.bufferedReader().use { it.readText() }
-            var helloTextView = findViewById<TextView>(R.id.text_view_id);
-            helloTextView.text = inputString;
+            var showBatteryInfo = findViewById<TextView>(R.id.text_view_id);
+
+            showBatteryInfo.text = inputString.replace("\n", "\n\n").replace("=", "=\n");
+            showBatteryInfo.movementMethod = ScrollingMovementMethod()
         } catch (e: Exception) {
         }
 
     }
+
+    private fun start(){
+        timer.start()
+    }
+    private fun stop(){
+        timer.cancel()
+    }
+    inner class Timer(miliis:Long) : CountDownTimer(miliis,1){
+        var millisUntilFinished:Long = 0
+        override fun onFinish() {
+        }
+        override fun onTick(millisUntilFinished: Long) {
+            this.millisUntilFinished = millisUntilFinished
+            val passTime = time - millisUntilFinished
+            val hour = passTime / 3600000 % 24
+            val min = passTime / 60000 % 60
+            var sec = (passTime / 1000 % 60).toInt()
+
+            val maxSec = 3
+
+            if((sec % maxSec) + 1 == maxSec) {
+                readFile()
+            }
+
+        }
+    }
+
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         readFile()
+        timer.start()
 
         var button = findViewById<Button>(R.id.button_id)
+
+
         button.setOnClickListener{
+            if(isStop == false) {
+                timer.start()
+                isStop = true
+
+                button.text = "Press to Stop"
+            } else {
+                timer.cancel()
+                isStop = false
+                button.text = "Press to Start"
+            }
+
             readFile()
         }
-
 
     }
 }
